85ffcd535960aad3a5e88ba99211a20b8ba86ed0
Connor Weng <foamtea30@gmail.com>
2015-12-29 16:49:34 +0800

    import data

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-var pageWidth = 800;
-var pageHeight = 600;
-var prevCx = 12;
-var prevCy = 12;
-var path = [{x1: prevCx, y1: prevCy, x2: prevCx, y2: prevCy}];
+var page = {"e":"pageload","t":236,"viewport":[1207,794],"screen":[1680,1010],"pos":[206,150],"b":"Microsoft Internet Explorer","bv":"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Tablet PC 2.0; .NET CLR 1.1.4322; .NET4.0C; .NET4.0E)"};
+var pageWidth = page['viewport'][0];
+var pageHeight = page['viewport'][1];
+var frameNo = 0;
 var svg = d3.select('#container')
-		.append('svg')
-		.attr('width', pageWidth)
-		.attr('height', pageHeight);
+      .append('svg')
+      .attr('width', pageWidth)
+      .attr('height', pageHeight);
 var mouse = svg.append('circle')
-		.attr('r', '6')
-        .attr('cx', prevCx)
-		.attr('cy', prevCy);
+      .attr('r', '4');
+var lines = svg.selectAll('line');
+var linesData = [];
 
 var render = function() {
-	var cx = Math.random() * 400;
-	var cy = Math.random() * 400;
-	mouse.attr('cx', cx)
-		.attr('cy', cy)
-	    .transition()
-	    .attr('r', '10')
-	    .transition()
-	    .attr('r', '6');
-	path.push({x1: prevCx, y1: prevCy, x2: cx, y2: cy});
-	svg.selectAll('line')
-		.data(path)
-		.enter()
-		.append('line')
-	    .attr('x1', function(d) { return d.x1; })
-		.attr('y1', function(d) { return d.y1; })
-		.attr('x2', function(d) { return d.x2; })
-	    .attr('y2', function(d) { return d.y2; })
-		.style('stroke', 'rgb(255,0,0)')
-		.style('stroke-width', '2');
-	prevCx = cx;
-	prevCy = cy;
+  mouse.attr('cx', data[frameNo].x)
+    .attr('cy', data[frameNo].y);
+
+  if (frameNo > 0) {
+    var prevPoint = data[frameNo-1];
+    var nowPoint = data[frameNo];
+    linesData.push({x1: prevPoint.x, y1: prevPoint.y, x2: nowPoint.x, y2: nowPoint.y});
+    lines.data(linesData)
+      .enter()
+      .append('line')
+      .style('stroke', 'rgb(255,0,0)')
+      .style("stroke-width", "2")
+      .attr('x1', function(d, i) {return d.x1;})
+      .attr('y1', function(d, i) {return d.y1;})
+      .attr('x2', function(d, i) {return d.x2;})
+      .attr('y2', function(d, i) {return d.y2;});
+  }
+
+  frameNo++;
 };
 
 var timer = setInterval(render, 400);
-setTimeout(function() {clearInterval(timer);}, 10000);
